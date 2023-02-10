5e62c18e5454b22f6f8da5f52da83f78e63e91dc
imanolof29 <imanolof29@gmail.com>
2023-02-10 18:44:46 +0100

    firebase

--- app/src/main/java/com/example/notas/presentation/details/components/InputField.kt
+++
@@ -1,9 +0,0 @@
-package com.example.notas.presentation.details.components
-
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.runtime.Composable
-
-@Composable
-fun InputField() {
-
-}
